691891ad7289b7ad760728e14cff01c1d08c2d94
ojisoekamti <abdulghoji31@gmail.com>
2023-08-23 00:47:14 +0700

    data comment Disable

--- src/data/single.ts
+++ src/data/single.ts
@@ -7,10 +7,7 @@ import { CommentType } from "components/CommentCard/CommentCard";
 import podcastImg from "images/podcast.jpg";
 
 // function nested the comment child -- make tree comment
-const nest = (
-  items: CommentType[],
-  id: number | null | undefined
-): CommentType[] => {
+const nest = (items: CommentType[], id: number | null | undefined): CommentType[] => {
   return items
     .filter((item) => item.parentId === id)
     .map((item) => ({
@@ -27,6 +24,7 @@ const commentHasAuthor = __comments.map((item) => ({
 
 //
 export const DEMO_COMMENTS = nest(commentHasAuthor, null);
+
 export const SINGLE: SinglePageType = {
   id: "eae0212192f63287e0c212",
   featuredImage:
@@ -77,7 +75,7 @@ export const SINGLE: SinglePageType = {
   postType: "standard",
   tags: [DEMO_TAGS[0], DEMO_TAGS[1], DEMO_TAGS[3]],
   content: "",
-  comments: DEMO_COMMENTS,
+  comments: [],
 };
 
 export const SINGLE_AUDIO: SinglePageType = {
@@ -125,8 +123,7 @@ export const SINGLE_AUDIO: SinglePageType = {
 
 export const SINGLE_VIDEO: SinglePageType = {
   id: "ea21ac32ds-6c192f68dscx7e0c212",
-  featuredImage:
-    "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  featuredImage: "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   title: "Julio Urías does it all as Dodgers sweep in San Francisco",
   desc: "We’re an online magazine dedicated to covering the best in international product design. We started as a little blog back in 2002 covering student work and over time",
   date: "May 20, 2021",
@@ -169,8 +166,7 @@ export const SINGLE_VIDEO: SinglePageType = {
 
 export const SINGLE_GALLERY: SinglePageType = {
   id: "eae0e85fd226c192f68dscx7e220c",
-  featuredImage:
-    "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  featuredImage: "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   title: "Julio Urías does it all as Dodgers sweep in San Francisco",
   desc: "We’re an online magazine dedicated to covering the best in international product design. We started as a little blog back in 2002 covering student work and over time",
   date: "May 20, 2021",
